10e671813086dbad07428babf2540e93196bdac2
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2022-05-10 05:24:58 +0500

    this is gonna be a bit challenging >o>

--- test.js
+++ test.js
@@ -2,14 +2,14 @@ const request = require("request");
 const fs = require("fs");
 const cheerio = require("cheerio");
 
-function WriteData(file, data) {
+function WriteData(data, file = "amazon.json") {
   fs.writeFile(file, JSON.stringify(data, null, 4), function (err) {
     console.log("File successfully written!");
   });
 }
 
-function scrape(url, file) {
-  request(url, function (error, response, html) {
+async function scrape(url, writeData = 1) {
+  await request(url, function (error, response, html) {
     if (!error) {
       let $ = cheerio.load(html, {
           normalizeWhitespace: true,
@@ -18,25 +18,40 @@ function scrape(url, file) {
         price = $(".priceBlock .price").text(),
         image = $(".imgTagWrapper img").attr("src"),
         description = $("#feature-bullets").html(),
-        data = {
+        obtainedData = {
           name: name,
           price: price,
           image: image,
           description: description,
         };
-      WriteData(file, data);
+      writeData == 1 && WriteData(obtainedData);
+      return obtainedData;
     }
   });
 }
 
-scrape(
-  "https://www.amazon.com/Apple-iPhone-12-Pro-Graphite/dp/B09JF5ZHQS/ref=sr_1_1?keywords=iphone&qid=1650875037&sr=8-1",
-  "amazon.json"
-);
+// scrape(
+//   "https://www.amazon.com/Apple-iPhone-12-Pro-Graphite/dp/B09JF5ZHQS/ref=sr_1_1?keywords=iphone&qid=1650875037&sr=8-1"
+// );
 
 // Multiple Data Scrapping
-function MultiScrape(url, file) {
-  for (let i = 0; i < url.length; i++) {
-    scrape();
-  }
+async function MultiScrape(links, filename = "amazon.json") {
+  const data = await Promise.all(
+    links.map(async (link) => {
+      fs.appendFileSync(filename, (await scrape(link)) + "\n", { flag: "a" });
+    })
+  );
+
+  // WriteData(data, filename);
 }
+
+var linksList = [];
+
+function getLinks(file) {
+  linksList = fs.readFileSync(file, "utf8").split("\n");
+  console.log(linksList);
+}
+
+getLinks("links.txt");
+
+MultiScrape(linksList);
